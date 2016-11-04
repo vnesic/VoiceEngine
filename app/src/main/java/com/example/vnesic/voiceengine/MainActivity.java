3d6fd8256ae56764a81f353f68e27979457e8a5e
vnesic <vladimirs.nesic@gmail.com>
2016-11-04 11:51:48 +0100

    voice engine
    voice to text

---
+++ app/src/main/java/com/example/vnesic/voiceengine/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.vnesic.voiceengine;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+    TextView t;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(Button) findViewById(R.id.button);
+        t=(TextView)findViewById(R.id.text);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displaySpeechRecognizer();
+            }
+        });
+    }
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            t.setText(spokenText);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
